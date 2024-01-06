68e2820e0a248e00efc2dd6273e9b7be7c774762
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-01-06 22:12:53 +0530

    Time: 1355 ms (51.96%), Space: 0B (100.00%) - LeetHub

---
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+WITH topSalesProductsInFeb AS (
+    SELECT 
+        product_id AS id, 
+        SUM(unit) AS total_units
+    FROM orders
+    WHERE order_date BETWEEN '2020-02-01' AND '2020-02-29'
+    GROUP BY product_id
+    HAVING SUM(unit) >= 100
+)
+
+SELECT 
+    p.product_name, 
+    ts.total_units AS unit
+FROM products p
+INNER JOIN topSalesProductsInFeb ts ON p.product_id = ts.id;
